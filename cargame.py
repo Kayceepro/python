89dc335f51caf3a4456cd1132fc66af4fdbe2937
kayceepro <nwabunwanne4u@gmail.com>
2023-01-16 11:32:16 +0100

    car simulation game without gui

--- cargame.py
+++ cargame.py
@@ -1,8 +1,13 @@
 command = ""
+started = False
 while command != "quit":
     command = input('> ').lower()
     if command == "start":
-        print('car started...Ready to go!')
+        if started:
+            print('car is already started')
+        else:
+            started = True
+            print('car started...Ready to go!')
     elif command == "help":
         print('''
         start     to start the car
@@ -10,6 +15,10 @@ while command != "quit":
         quit       to end the game
         ''')
     elif command == "stop":
+        if not started:
+            print('car has already stopped')
+        else:
+            started = False
         print('car stopped.')
     elif command == "quit":
         break
