7b3425716209e5d2e119af16672c92b0ae8471e9
kayceepro <nwabunwanne4u@gmail.com>
2023-01-16 21:20:01 +0100

    dian

---
+++ hello1.py
@@ -0,0 +1,17 @@
+print("hello world")
+for x in range(4):
+  print(x)
+x = 0
+while x < 4:
+  for y in range (4):
+    print(f"{x} is kaycee {y} is Dozie")
+  
+  x += 1
+  
+count = 2
+count = count + 1
+print(count)
+count += 1
+print(count)
+#count++
+print(count)
