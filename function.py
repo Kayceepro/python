3d39b0251898077ca11e4860d98abce1ee29f9b8
kayceepro <nwabunwanne4u@gmail.com>
2023-01-17 00:01:03 +0100

    commit is empty

---
+++ function.py
@@ -0,0 +1,8 @@
+def greet_user(first_name, last_name):
+    print(f"hi {first_name} {last_name}")
+    print("welcome on board")
+    
+    
+print('start')
+print(greet_user("kelechi",  last_name="Nwabunwanne"))
+print("stop")
